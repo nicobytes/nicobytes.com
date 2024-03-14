53cda043b69848470d85641f7405c9f78ef232f9
Nicolas Molina Monroy <hi@nicobytes.com>
2024-03-14 11:50:34 -0400

    Update package.json and remove unused code

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,6 @@ import tailwind from '@astrojs/tailwind';
 import compressor from 'astro-compressor';
 import sitemap from '@astrojs/sitemap';
 import robotsTxt from 'astro-robots-txt';
-import { VitePWA } from 'vite-plugin-pwa';
-import { manifest } from './src/utils/manifest';
 
 import partytown from '@astrojs/partytown';
 
@@ -47,18 +45,5 @@ export default defineConfig({
         forward: ['dataLayer.push'],
       },
     }),
-  ],
-  vite: {
-    plugins: [
-      VitePWA({
-        registerType: 'autoUpdate',
-        manifest,
-        workbox: {
-          globDirectory: 'dist',
-          globPatterns: ['**/*.{js,css,svg,png,jpg,jpeg,gif,webp,woff,woff2,ttf,eot,ico}'],
-          navigateFallback: null,
-        },
-      }),
-    ],
-  },
+  ]
 });
